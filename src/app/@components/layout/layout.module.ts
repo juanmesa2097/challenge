f05f8e52674be890e9930eef1c386c3a2dc232d3
juanmesa2097 <juanmesa2097@gmail.com>
2021-03-26 12:14:22 -0500

    chore: add footer

--- src/app/@components/layout/layout.module.ts
+++ src/app/@components/layout/layout.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FooterModule } from "../footer/footer.module";
 import { NavbarModule } from "../navbar/navbar.module";
 import { LayoutComponent } from "./layout.component";
 
 @NgModule({
   declarations: [LayoutComponent],
-  imports: [CommonModule, NavbarModule],
+  imports: [CommonModule, NavbarModule, FooterModule],
   exports: [LayoutComponent],
 })
 export class LayoutModule {}
